a0ca444fe162f8a7ad984ad974348266eb8ce5f5
Jeongzmin <jeongpard024@gmail.com>
2024-08-31 01:41:07 +0900

    fix - opensearch

---
+++ docker/opensearch/opensearch-2.16.0-2node/conf/opensearch-plungins-install.sh
@@ -0,0 +1,7 @@
+#chmod +x ./opensearch/opensearch-plungins-install.sh
+
+echo "opensearch-plungins-install.sh start"
+
+./bin/opensearch-plugin install analysis-nori
+
+echo "opensearch-plungins-install.sh end"
---
+++ docker/opensearch/opensearch-2.16.0-2node/read.md
@@ -0,0 +1,3 @@
+
+sudo chmod -R 755 /data/opensearch-dir/data1
+sudo chmod -R 755 /data/opensearch-dir/data2
